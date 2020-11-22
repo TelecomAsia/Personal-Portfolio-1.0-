9a7d9ecdfc0e8d11e680899883ef159eab769bd9
TelecomAsia <111300933+TelecomAsia@users.noreply.github.com>
2020-11-22 20:45:58 +0000

    Update folder structure

--- app.js
+++ app.js
@@ -4,4 +4,24 @@ import Combos from "./src/controllers/Combos.js"
 import Salas from "./src/controllers/Sala.js";
 import Filmes from "./src/controllers/Filmes.js"
 //import FilmesMetodos from "./src/DAO/FilmesMetodos.js";
-import Ingresso from
+import Ingresso from "./src/controllers/Ingresso.js"
+import cors from 'cors'
+
+dotenv.config()
+
+const port = process.env.PORT || 3002;
+const app = express()
+
+app.listen(port, () => {
+    console.log(`servidor online em http://localhost:${port}`)
+})
+
+app.use(express.json())
+app.use(cors())
+
+//FilmesMetodos.createTableFilmes()
+
+Combos.combos(app)
+Salas.rotas(app)
+Filmes.rotas(app)
+Ingresso.rotas(app)
