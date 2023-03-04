f7570a538af597345701a55d0088b5c8d2539ead
TelecomAsia <111300933+TelecomAsia@users.noreply.github.com>
2023-03-04 12:53:49 +0000

    removing log

--- src/DAO/FilmesMetodos.js
+++ src/DAO/FilmesMetodos.js
@@ -2,4 +2,21 @@ import DAO from "./DAO.js"
 // import Database from "../infra/Database.js"
 
 class FilmesMetodos extends DAO{
-    static
+    static async createTableFilmes() {
+        const query = `
+        CREATE TABLE IF NOT EXISTS filmes(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome VARCHAR,
+            genero VARCHAR,
+            duracao VARCHAR,
+            anoLancamento VARCHAR,
+            diretor VARCHAR,
+            sinopse TEXT,
+            image TEXT
+        )`
+            const response = await this.createTable(query)
+            return response
+    }
+
+    static async inserirFilme(filme) {
+        const query = `INSERT INTO filmes (nome, genero, dura
