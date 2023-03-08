45d12bce786902966b007fe81daea87806a9639b
TelecomAsia <111300933+TelecomAsia@users.noreply.github.com>
2023-03-08 05:38:56 +0000

    Remove unused package.

--- src/controllers/Filmes.js
+++ src/controllers/Filmes.js
@@ -11,4 +11,18 @@ class Filmes{
             res.status(200).json(response)
         })
 
- 
+        app.get("/filmes/:id", async (req, res) => {
+            try {
+                const filme = await FilmesMetodos.listarFilmePorId(req.params.id)
+                if(!filme){
+                    throw new Error("Filme não encontrado para o id")
+                }
+                res.status(200).json(filme)
+            } catch (error) {
+                res.status(404).json(error.message)
+                
+            }
+        })
+
+        app.post("/filmes", async (req, res) => {
+    
