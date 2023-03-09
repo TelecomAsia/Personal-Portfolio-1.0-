59ef14499c1b84cb4e267b941b9208343292cc18
TelecomAsia <111300933+TelecomAsia@users.noreply.github.com>
2023-03-09 07:01:46 +0000

    Updated the dask feedstock.

--- src/controllers/Ingresso.js
+++ src/controllers/Ingresso.js
@@ -8,4 +8,16 @@ class Ingresso {
     static rotas(app) {
         app.get('/ingresso', async (req, res) => {
             try {
-                const response = await IngressoMetodos.listarTodosIngr
+                const response = await IngressoMetodos.listarTodosIngressos()
+                res.status(200).json(response)
+            } catch (error) {
+                res.status(400).json(error.message)
+            }
+        })
+        app.get('/ingresso/:id', async (req, res) => {
+            try {
+                const ingresso = await IngressoMetodos.listarIngressoPorId(req.params.id)
+                if (!ingresso) {
+                    throw new Error("Ingresso não identificado em nosso sistema.");
+                }
+                res.status(200).json(ingresso)
