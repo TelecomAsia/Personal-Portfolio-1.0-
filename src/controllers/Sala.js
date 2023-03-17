b5cc981f37bc692074c33e8ac02cc09f809e5674
TelecomAsia <111300933+TelecomAsia@users.noreply.github.com>
2023-03-17 12:27:17 +0000

    added extensions

--- src/controllers/Sala.js
+++ src/controllers/Sala.js
@@ -12,4 +12,17 @@ class Salas {
             res.status(200).json(response)
         })
 
-        app.get("/sala/:id", asy
+        app.get("/sala/:id", async (req, res) => {
+            try {
+                const Sala = await DatabaseSalaMetodos.listarSalaPorId(req.params.id)
+                if (!Sala) {
+                    throw new Error("Não encontradamos nenhuma sala com esse Id")
+                }
+                res.status(200).json(Sala)
+            } catch (error) {
+                res.status(404).send(error.message)
+            }
+        })
+
+        app.post("/sala", async (req, res) => {
+            const isValid = ValidacoesSala.isValid(...Object.values(req.bod
