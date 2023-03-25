7b12242138327b73f1548ca1bcb500cb45b1be46
TelecomAsia <111300933+TelecomAsia@users.noreply.github.com>
2023-03-25 09:06:32 +0000

    Fix indentation

--- src/services/ValidacoesCombos.js
+++ src/services/ValidacoesCombos.js
@@ -4,4 +4,28 @@ export default class ValidacoesCombos {
      * @param {String} name 
      * @returns boolean
      */
-    static validaName(name){
+    static validaName(name){                                   
+        if (name.length >= 4 && name.length <= 12){
+            return true
+        } 
+        else{
+            return false
+        }
+    }
+
+    /**
+     * 
+     * @param {String} item1 
+     * @param {String} item2 
+     * @param {String} item3 
+     * @returns boolean
+     */
+    static validaItens(...itens){
+        const itensOriginais = [...itens]
+        const itensFilter = [... new Set (itensOriginais)]
+        return itensOriginais.length == itensFilter.length
+    }
+
+    /**
+     * 
+     * @param {float} pric
