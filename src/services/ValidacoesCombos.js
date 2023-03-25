1a6e5e9181de829999d4677b22ec0179ae6c797c
TelecomAsia <111300933+TelecomAsia@users.noreply.github.com>
2023-03-25 21:19:25 +0000

    Released v0.5.0

--- src/services/ValidacoesCombos.js
+++ src/services/ValidacoesCombos.js
@@ -28,4 +28,25 @@ export default class ValidacoesCombos {
 
     /**
      * 
-     * @param {float} pric
+     * @param {float} price 
+     * @returns boolean
+     */
+    static validaPreco(price) {
+        if (price <=60 && price >= 15) {
+            return true;
+        }
+        else{
+            return false
+        }
+    }
+
+    /**
+     * @param {String} item1
+     * @param {String} item2
+     * @param {String} item3
+     * @returns boolean
+     */
+    static validaCombos(name, price, item1, item2, item3){
+        return this.validaName(name)
+            && this.validaPreco(price)
+            && this.validaItens(item1, item2, it
