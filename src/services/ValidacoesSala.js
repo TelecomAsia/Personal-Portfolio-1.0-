a15790e056ca8d646ee73032f9d0c11690ddd2af
TelecomAsia <111300933+TelecomAsia@users.noreply.github.com>
2023-03-26 12:47:02 +0000

    Fixed compilation warning

--- src/services/ValidacoesSala.js
+++ src/services/ValidacoesSala.js
@@ -17,4 +17,28 @@ class ValidacoesSala extends ValidacoesService {
      */
     static validaCertificadoDeVistoriaAnual(certificado) {
         if (typeof certificado != "boolean") {
-           
+            return false
+        } else {
+            return true
+        }
+    }
+
+    /**
+     * 
+     * @param {string} tipo
+     * @returns boolean 
+     */
+    static validaCategoriaDaSala(tipo) {
+        let tipos = tipo.toLowerCase()
+        switch (tipos) {
+            case 'imax':
+            case 'xd':
+            case 'macro xe':
+            case 'xplus':
+            case 'reald':
+            case 'd-box':
+                return true
+            default:
+                return false
+        }
+    }
