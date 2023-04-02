3f6d7d8974b28bb517e9921490d29364640c6fe9
TelecomAsia <111300933+TelecomAsia@users.noreply.github.com>
2023-04-02 14:40:23 +0000

    update parent dependency

--- src/test/ValidacoesSala.test.js
+++ src/test/ValidacoesSala.test.js
@@ -2,4 +2,20 @@ import ValidacoesSala from "../services/ValidacoesSala.js"
 
 
 test("Validar se o número de cadeiras namoradeiras é um número inteiro", ()=>{
-    expect(ValidacoesSala.validaCadeira(1.45)).toBe
+    expect(ValidacoesSala.validaCadeira(1.45)).toBe(false)
+})
+
+test("Validar se o número de cadeiras comuns é um número inteiro", ()=>{
+    expect(ValidacoesSala.validaCadeira(false)).toBe(false)
+}) 
+
+
+test("Validar se o número de espaços disponíveis para acesso de cadeirantes é um número inteiro", ()=>{
+    expect(ValidacoesSala.validaCadeira(5/2)).toBe(false)
+}) 
+
+test("Validar se a sala está com o certificado de vistoria anual em dia ou não", ()=>{
+    expect(ValidacoesSala.validaCertificadoDeVistoriaAnual("vedadeiro")).toBe(false)
+}) 
+
+te
