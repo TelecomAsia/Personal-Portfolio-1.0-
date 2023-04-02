8684e78cad04d7a063282501d899b9bb0b9d264a
TelecomAsia <111300933+TelecomAsia@users.noreply.github.com>
2023-04-02 18:23:14 +0000

    RAS

--- src/test/ValidacoesSala.test.js
+++ src/test/ValidacoesSala.test.js
@@ -18,4 +18,14 @@ test("Validar se a sala está com o certificado de vistoria anual em dia ou não
     expect(ValidacoesSala.validaCertificadoDeVistoriaAnual("vedadeiro")).toBe(false)
 }) 
 
-te
+test("Validar se a sala está com o certificado de vistoria anual em dia ou não", ()=>{
+    expect(ValidacoesSala.validaCertificadoDeVistoriaAnual(true)).toBe(true)
+}) 
+
+test("Validar se a categoria da sala está entre os tipos existentes e disponíveis", ()=>{
+    expect(ValidacoesSala.validaCategoriaDaSala("imas")).toBe(false)
+}) 
+
+test("Validar se a categoria da sala está entre os tipos existentes e disponíveis", ()=>{
+    expect(ValidacoesSala.validaCategoriaDaSala("imax")).toBe(true)
+}) 
